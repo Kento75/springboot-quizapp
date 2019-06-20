febbff203aa7ccf9a827bac4b6a6f521f10528a4
Kento75 <kento2github@gmail.com>
2019-06-20 21:39:54 +0900

    Add check route mock

--- src/main/java/com/example/quizapp/QuizAppController.java
+++ src/main/java/com/example/quizapp/QuizAppController.java
@@ -13,16 +13,36 @@ public class QuizAppController {
 
     private List<Quiz> quizzes = new ArrayList<>();
 
+    // クイズ一覧表示
     @GetMapping("/show")
     public List<Quiz> show() {
         return quizzes;
     }
 
+    // クイズ作成
     @PostMapping("/create")
     public void create(@RequestParam String question, @RequestParam boolean answer) {
         Quiz quiz = new Quiz(question, answer);
 
         // クイズを追加
         quizzes.add(quiz);
+    }
+
+    // クイズ回答
+    @GetMapping("/check")
+    public String check(@RequestParam String  question, @RequestParam boolean answer) {
+        // TODO: 回答が正しいかどうかチェックして、結果を返却
+        // 指定されたquestionを登録済みのクイズから検索
+        for(Quiz quiz : quizzes) {
+            // クイズが見つかった場合
+            if (quiz.getQuestion().equals(question)) {
+                // 正解の場合
+                return "見つかった!：" + quiz.getQuestion();
+                // 不正解の場合
+
+            }
+        }
+        // クイズが見つからなかった場合
+        return "";
     }
 }
