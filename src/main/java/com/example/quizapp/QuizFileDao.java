ecd14b7a3c18f3196616cc6d1b01d50ae18352dd
Kento75 <kento2github@gmail.com>
2019-06-25 14:22:17 +0900

    Add quizzes file read method

--- src/main/java/com/example/quizapp/QuizFileDao.java
+++ src/main/java/com/example/quizapp/QuizFileDao.java
@@ -27,4 +27,18 @@ public class QuizFileDao {
         // 第二引数：書き込むデータ(List<String>)
         Files.write(path, lines);
     }
+
+    // クイズファイル読み込み処理
+    public List<Quiz> read() throws IOException {
+        Path path = Paths.get(FILE_PATH);
+        List<String> lines = Files.readAllLines(path);
+
+        List<Quiz> quizzes = new ArrayList<>();
+
+        for(String line : lines) {
+            quizzes.add(Quiz.fromString(line));
+        }
+
+        return quizzes;
+    }
 }
